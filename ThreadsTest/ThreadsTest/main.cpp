84129ebce48adb179e822be8d34ab50d05059709
realEricBrun <ericbrun350@att.net>
2019-06-23 15:57:18 -0500

    Implemented and tested recursive fibonacci function.

--- ThreadsTest/ThreadsTest/main.cpp
+++ ThreadsTest/ThreadsTest/main.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <string>
+#include <thread>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void pause()
+{
+	char ch;
+	std::string str = "PRess any key to continue...";
+	cout << str;
+	cin >> ch;
+}
+
+//thread 1
+//runs a recursive fibonacci function and prints outputs
+int fib(int start)
+{
+		if(start == 0 || start == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return fib(start - 1) + fib(start - 2);
+		}
+}
+
+void fibWrapper(int totalRuns)
+{
+	int i = 0;
+	while(i < totalRuns)
+	{
+		int n = fib(i);
+		cout << "Fib(" << i << ") = " << n << endl;
+		++i;
+	}
+}
+
+//thread 2
+//runs a recursive ackermann function and prints outputs
 
 int main()
 {
+	int goal = 10;
+	fibWrapper(goal);
 
+	pause();
 
 	return 0;
 }
