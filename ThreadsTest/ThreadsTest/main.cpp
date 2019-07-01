31515d0aaf3fc5127161278dada6e3814747528e
realEricBrun <ericbrun350@att.net>
2019-06-30 19:05:39 -0500

    Separated display functions from threaded functions.

--- ThreadsTest/ThreadsTest/main.cpp
+++ ThreadsTest/ThreadsTest/main.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
 
 void pause()
 {
 	char ch;
-	std::string str = "PRess any key to continue...";
+	std::string str = "Press any key to continue...";
 	cout << str;
 	cin >> ch;
+}
+
+void printFibResults(vector<int> results)
+{
+	for (int i = 0; i < results.size(); ++i)
+	{
+		cout << "Fib("<< i << ") = " << results.at(i) << endl;
+	}
+}
+
+void printAckResults(vector<int> results, int endX, int endY)
+{
+	int x = 0, y = 0, i = 0;
+	while (i < results.size())
+	{
+		cout << "Ack(" << x << ", " << y << ") = " << results.at(i) << endl;
+		++y;
+		if(y > endY)
+		{
+			++x; y = 0;
+		}
+		++i;
+	}
 }
 
 //thread 1
@@ -28,18 +53,13 @@ int fib(int start)
 		}
 }
 
-void fibPrint(int i, int n)
-{
-	cout << "Fib(" << i << ") = " << n << endl;
-}
-
-void fibWrapper(int totalRuns)
+void fibWrapper(int totalRuns, vector<int>& results)
 {
 	int i = 0;
 	while(i < totalRuns)
 	{
 		int n = fib(i);
-		fibPrint(i, n);
+		results.push_back(n);
 		++i;
 	}
 }
@@ -66,18 +86,13 @@ int ack(int a, int b)
 	}
 }
 
-void ackPrint(int i, int j, int n)
-{
-	cout << "Ack(" << i << ", " << j << ") = " << n << endl;
-}
-
-void ackWrapper(int totalX, int totalY)
+void ackWrapper(int totalX, int totalY, vector<int>& results)
 {
 	int i = 0, j = 0;
 	while (i <= totalX && j <= totalY)
 	{
 		int n = ack(i, j);
-		ackPrint(i, j, n);
+		results.push_back(n);
 		++j;
 		if (j % totalY == 0)
 		{
@@ -91,15 +106,23 @@ int main()
 {
 	int fibGoal = 25;
 //	fibWrapper(fibGoal);
+	vector<int> fibResults;
 
 	int ackGoalX = 3, ackGoalY = 10;
 //	ackWrapper(ackGoalX, ackGoalY);
+	vector<int> ackResults;
 
-	std::thread fibThread(fibWrapper, fibGoal);
-	std::thread ackThread(ackWrapper, ackGoalX, ackGoalY);
+	cout << "Starting threads..." << endl;
+	std::thread fibThread(fibWrapper, fibGoal, std::ref(fibResults));
+	std::thread ackThread(ackWrapper, ackGoalX, ackGoalY, std::ref(ackResults));
+
 
 	ackThread.join();
 	fibThread.join();
+	cout << "Threads are done running..." << endl;
+
+	printFibResults(fibResults);
+	printAckResults(ackResults, ackGoalX, ackGoalY);
 
 	pause();
 
